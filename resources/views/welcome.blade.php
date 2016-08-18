85d72b4d1e4b54122119cd4db3773fcbf32eb593
rafaelescrich <rafaelescrich@gmail.com>
2016-08-17 22:02:48 -0300

    Added some information to the welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,12 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Bem-vindo</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <h3>Clique no link abaixo para visualizar a lista de usuários.</h3>
+
+                    <div class="btn btn-primary text-center">{{ link_to_route('user.index', 'Lista de usuários', null, ['class'=>'btn btn-primary']) }}</div>
                 </div>
             </div>
         </div>
